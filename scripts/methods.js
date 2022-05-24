70f4d265c06a5ebea8e6745cbfadb9b67a8e7a43
AdriCBueno <58597644+adCBueno@users.noreply.github.com>
2022-05-24 10:15:44 -0400

    nnn1

---
+++ scripts/methods.js
@@ -0,0 +1,89 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const selectDrop = document.querySelector('#countries');
+    // const selectDrop = document.getElementById('countries');
+  
+  
+    fetch('https://restcountries.com/v2/lang/es').then(res => {
+      return res.json();
+    }).then(data => {
+      let output = "";
+      data.forEach(country => {
+        output += `
+        
+        <option value="${country.name}">${country.name}</option>`;
+      })
+  
+      selectDrop.innerHTML = output;
+    }).catch(err => {
+      console.log(err);
+    })
+  
+  
+  });
+
+// Conver URL API
+var requestURL = 'https://api.exchangerate.host/latest';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  var response = request.response;
+  console.log(response);
+}
+
+// for Currency
+var requestURL = 'https://restcountries.com/v2/lang/es';
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  var response = request.response;
+  console.log(response);
+}
+
+const monedaEl_one = document.getElementById('moneda-uno');
+const monedaEl_two = document.getElementById('moneda-dos');
+const cantidadEl_one = document.getElementById('cantidad-uno');
+const cantidadEl_two = document.getElementById('cantidad-dos');
+const cambioEl = document.getElementById('cambio');
+const tazaEl = document.getElementById('taza');
+
+
+// Fetch Exchange Rate and Update the DOM
+function calculate(){
+    const moneda_one = monedaEl_one.value;
+    const moneda_two = monedaEl_two.value;
+
+   fetch(`https://api.exchangerate-api.com/v4/latest/${moneda_one}`)
+   .then(res => res.json() )
+   .then(data => {
+       const taza = data.rates[moneda_two];
+       
+       cambioEl.innerText = `1 ${moneda_one} = ${taza} ${moneda_two}`;
+
+       cantidadEl_two.value = (cantidadEl_one.value * taza).toFixed(2);
+
+    } );
+    
+}
+
+//Event listeners
+monedaEl_one.addEventListener('change', calculate);
+cantidadEl_one.addEventListener('input', calculate);
+monedaEl_two.addEventListener('change', calculate);
+cantidadEl_two.addEventListener('input', calculate);
+
+taza.addEventListener('click', () =>{
+    const temp = monedaEl_one.value;
+    monedaEl_one.value = monedaEl_two.value;
+    monedaEl_two.value = temp;
+    calculate();
+} );
+
+
+calculate();
